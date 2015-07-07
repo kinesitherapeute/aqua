819aa24f59762810087744f4096e5e8e5d70612e
Pierre Besson <pierre.besson7@gmail.com>
2015-07-07 18:23:32 +0200

    [cabinet] Add cabinet content.

---
+++ src/data/cabinet.js
@@ -0,0 +1,12 @@
+let data = {
+  "name": "Cabinet de kinésitherapie Besson Ansart",
+  "phone": "0320882945",
+  "map": "http://google.fr",
+  "photo": null,  
+  "address":{
+    "number": "2",
+    "street": "Rue camille Saint Saens"
+  }
+};
+
+export default data;
--- src/view/layout.js
+++ src/view/layout.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react';
 import Team from './team'
+import Cabinet from './cabinet'
 import teamData from '../data/team'
+import cabinetData from '../data/cabinet'
 export default class Layout extends Component {
   render() {
     let { title } = this.props;
     return (
       <div className='layout'>
         <h1>{title}</h1>
+        <Cabinet {...cabinetData} />
         <Team list={teamData}/>
       </div>
     );
