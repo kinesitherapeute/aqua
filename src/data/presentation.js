1b2a3117f257849ba428738fa6040f6a0e5bbbc3
Pierre Besson <pierre.besson7@gmail.com>
2015-12-14 15:57:17 +0100

    [data] Add static data

---
+++ src/data/presentation.js
@@ -0,0 +1,14 @@
+export default {
+    kine: {
+        title: 'kiné',
+        content: 'Super kine'
+    },
+    balneo:{
+        title: 'kiné',
+        content: 'Super balnéo'
+    },
+    aquaBike: {
+        title: 'Aqua Bike',
+        content: 'Super aqua bike'
+    }
+}
--- src/data/team.js
+++ src/data/team.js
@@ -1,7 +1,5 @@
-let data = [
+export default [
   {"id": 1, "firstName": "Catherine", "lastName": "Besson", "photo":null, "skills": "kine respi"},
   {"id": 2, "firstName": "Jacques", "lastName": "Besson", "photo":null, "skills": "top kine"},
   {"id": 3, "firstName": "Olivier", "lastName": "XXXXX", "photo":null, "skills": "new kine"}
 ];
-
-export default data;
