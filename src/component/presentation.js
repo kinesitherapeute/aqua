9180e56d8f626e17ad0b6d29090c8f63dd048f1c
Pierre Besson <pierre.besson7@gmail.com>
2015-12-16 12:10:51 +0100

    [style]

--- src/component/presentation.js
+++ src/component/presentation.js
@@ -1,8 +1,10 @@
 import React from 'react';
-import {Card, CardHeader, CardMedia, CardText, CardTitle} from 'material-ui/lib/card';
-import {presentation as presentationStyle, presentationCard as cardStyle} from './style';
-function PresentationCard({title, content}){
-    return <div><Card style={{height: '100%'}}><CardTitle title={title}/><CardText>{content}</CardText></Card></div>;
+import {Card, CardHeader, CardMedia, CardText, CardTitle, CardActions} from 'material-ui/lib/card';
+import PhoneIcon from 'material-ui/lib/svg-icons/communication/screen-share';
+
+
+function PresentationCard({title, content, action}){
+    return <div><Card ><CardTitle title={title}/><CardMedia overlay={<CardTitle subtitle={content}><PhoneIcon /></CardTitle>}><img src='http://lorempixel.com/400/200/people/'/></CardMedia><CardActions><button>hi</button></CardActions></Card></div>;
 }
 
 function Presentation({kine, balneo, aquaBike}){
--- src/index.js
+++ src/index.js
@@ -4,7 +4,12 @@ import App from './App';
 
 // data
 const TITLE = 'Aqua kine sports';
-const intro= 'Bienvenue chez Aqua Kine sport......................'
+const intro= `Aqua kiné sport est un cabinet de kinésithérapie situé à Ronchin.
+Ce centre est né de l'ambition de proposer un lieu de rééducation adapté à la prise en charge des sportifs de haut niveau et de mettre ce matériel et ces compétences à la disposition du plus grand nombre.
+
+Avec une simple ordonnance médicale, vous avez donc accès à une rééducation optimale.
+Notre secrétaire  vous accueille du lundi au vendredi de 8h30 à 12h30 et de 14h15 à 17h15, (14h à 16h le mercredi)
+`
 import teamData from './data/team';
 import cabinetData from './data/cabinet';
 import presentationData from './data/presentation';
