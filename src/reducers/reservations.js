6864b3a75a71cccf840050ad2d73e932aaf9cdf6
Pierre Besson <pierre.besson7@gmail.com>
2015-12-23 12:23:31 +0100

    [reducers]

--- src/reducers/reservations.js
+++ src/reducers/reservations.js
@@ -1,14 +1,23 @@
 import {RESERVATION_ADD, RESERVATION_REMOVE} from '../actions/constants';
 
-function reservationsReducer(state = [], action){
+const DEFAULT_RESERVATION = [{title: 'test',
+begin: new Date(),
+end: new Date(),
+description: 'Test desc ....'
+}, {title: 'test2',
+begin: new Date(),
+end: new Date(),
+description: 'Test 2 desc ....'
+}];
+export default function reservationsReducer(state = DEFAULT_RESERVATION, action){
     const {type, payload} = action;
     switch(type){
         case RESERVATION_ADD:
-            return [...state, payload];
+        return [...state, payload];
         break;
         case RESERVATION_REMOVE:
-            const removeIdx = state.findIndex(r => r.id === payload);
-             return [...state.slice(0,removeIdx), ...state.slice(removeIdx+1)]
+        const removeIdx = state.findIndex(r => r.id === payload);
+        return [...state.slice(0,removeIdx), ...state.slice(removeIdx+1)]
         break;
     }
     return state;
