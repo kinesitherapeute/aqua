62d705cc864215d3ee02d13a5288e3a2df6e2908
Pierre Besson <pierre.besson7@gmail.com>
2015-12-23 12:23:52 +0100

    [reservation actions]

--- src/actions/reservations.js
+++ src/actions/reservations.js
@@ -1,8 +1,12 @@
-import {RESERVATION_ADD, RESERVATION_REMOVE} from './constants';
+import {RESERVATION_ADD, RESERVATION_REMOVE, RESERVATION_CURRENT_SET} from './constants';
 
 export function addReservation(reservation){
     return {payload: reservation, type: RESERVATION_ADD};
 }
 export function removeReservation(reservationId){
     return {payload: reservationId, type: RESERVATION_REMOVE};
+}
+
+export function setCurrentReservation(reservationId){
+    return {payload: reservationId, type: RESERVATION_CURRENT_SET};
 }
